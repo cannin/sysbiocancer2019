a0cd7648ca96ece7963a281f0cdd9e1c1c89d292
cannin <cannin@gmail.com>
2019-09-21 21:15:02 +0200

    Updated installation files

--- .binder/install.R
+++ .binder/install.R
@@ -1,5 +1,6 @@
 install.packages("remotes")
+install.packages("git2r")
 install.packages("rJava")
 
 source("https://gist.githubusercontent.com/cannin/6b8c68e7db19c4902459/raw/installPackages.R")
-installPackages("r-requirements.dcf")
+installPackages(".binder/r-requirements.dcf")
